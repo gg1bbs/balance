a9d2355b858b1465df8b5a183e9b01dcd2e705d6
gg1bbs <54209181+gg1bbs@users.noreply.github.com>
2022-09-26 23:51:54 +1000

    fixed physics I think

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ pygame.init()
 
 FPS = 60
 FramePerSec = pygame.time.Clock()
+beam_length = 1
+u = 1
 
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
@@ -37,22 +39,19 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (300, 200)
 
-
 def loop():
-
     g = 9.8
     m = 100
     x = 0
     v = 0
     a = 0
     ang = 0
-    adj = 1
+    adj = 0.5
 
     P1 = Ball()
-    # B1 = Beam()
+
     all_sprites = pygame.sprite.Group()
     all_sprites.add(P1)
-    # all_sprites.add(B1)
 
     # Simulation Loop
     while True:
@@ -66,6 +65,8 @@ def loop():
             ang = ang + adj/FPS
         if pressed_keys[K_RIGHT]:
             ang = ang - adj/FPS
+        if pressed_keys[K_b]:
+            ang = 0
 
         if ang < -math.pi/2:
             ang = -math.pi/2
@@ -74,7 +75,7 @@ def loop():
 
         g, m, x, v, a, ang, adj = simulation(g, m, x, v, a, ang, adj)
 
-        print(f"x = {x}, angle = {ang *180/math.pi}")
+        # print(f"x = {x}, angle = {ang *180/math.pi}")
 
         DISPLAYSURF.fill(WHITE)
 
@@ -107,31 +108,51 @@ def simulation(g, m, x, v, a, ang, adj):
     # elif ang > math.pi/4:
     #     ang = math.pi/4
 
-    a += -1 * (math.sin(ang) * g) / FPS
-    print(math.sin(ang))
-    v += a / FPS
-    x += v / FPS
-    if x < -1:
-        x = -1
-        v = 0
-        a = 0
-    elif x > 1:
-        x = 1
-        v = 0
-        a = 0
+    #gravity acceleration
+    
+    a_g = -1 * (math.sin(ang) * g)
+    
+    # friction acceleration
+    a_f = 0
+    if v > 0:
+        a_f -= (u * math.cos(ang) * g)
+    
+    elif v < 0:
+        a_f += (u * math.cos(ang) * g)
 
-    if a > 0:
-        a -= 0.004 * math.cos(ang) * g
-    elif a < 0:
-        a += 0.004 * math.cos(ang) * g
+    #adjust velocity due to gravitational force
+    v += a_g / FPS
+    
+    #save velocity before frictional acceleration is added
+    v_before_f = v
+
+    #adjust velocity due to frictional force
+    v += a_f / FPS
+
+    #see if grvitational force is sufficent to flip velocity sign, if so set velocity to zero
+    if flipped_sign(v_before_f, v):
+        v = 0
+
+    x += v / FPS
+    
+    # define beam length
+    if x < -beam_length/2:
+        x = -beam_length/2
+        v = 0
+        a = 0
+    elif x > beam_length/2:
+        x = beam_length/2
+        v = 0
+        a = 0
 
     return g, m, x, v, a, ang, adj
 
 
 def ball_pos(x, ang):
+    
     if ang != 0:
-        ball_x = x * math.cos(ang) * 200 + SCREEN_WIDTH/2
-        ball_y = - x*math.sin(ang) * 200 + SCREEN_HEIGHT/2
+        ball_x = (x/beam_length) * math.cos(ang) * 400 + SCREEN_WIDTH/2
+        ball_y = - (x/beam_length) * math.sin(ang) * 400 + SCREEN_HEIGHT/2
     else:
         ball_x = x * 200 + SCREEN_WIDTH/2
         ball_y = SCREEN_HEIGHT/2
@@ -144,18 +165,22 @@ def ball_pos(x, ang):
 
 def beam_pos(ang):
 
-    beam_length = 200
-
-    beam_start_x = SCREEN_WIDTH/2 - beam_length*math.cos(-ang)
-    beam_start_y = SCREEN_HEIGHT/2 - beam_length*math.sin(-ang)
-    beam_end_x = SCREEN_WIDTH/2 + beam_length*math.cos(-ang)
-    beam_end_y = SCREEN_HEIGHT/2 + beam_length*math.sin(-ang)
+    draw_beam_length = 200
+    beam_start_x = SCREEN_WIDTH/2 - draw_beam_length*math.cos(-ang)
+    beam_start_y = SCREEN_HEIGHT/2 - draw_beam_length*math.sin(-ang)
+    beam_end_x = SCREEN_WIDTH/2 + draw_beam_length*math.cos(-ang)
+    beam_end_y = SCREEN_HEIGHT/2 + draw_beam_length*math.sin(-ang)
 
     beam_start = (beam_start_x, beam_start_y)
     beam_end = (beam_end_x, beam_end_y)
 
     return beam_start, beam_end
 
+#return 1 if sign is flipped between two elements
+def flipped_sign(a, b):
+    if ((a == b) & (a==0)) | (a*b>0):
+        return 0
+    return 1
 
 if __name__ == "__main__":
     main()
